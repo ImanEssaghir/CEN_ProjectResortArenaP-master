ea602b5d67cf70559d03a80cfa09023ab12f05de
gianniperez <36053371+giannimperez@users.noreply.github.com>
2019-11-02 00:24:14 -0400

    Added resortDatabase and tables (EVENTS and RESORTGUESTS). DB files are in new "res" folder in project structure. Database Connection Test inside Main.java.

--- CEN_ProjectResortArenaP-master/src/ResortArenaPalace/Main.java
+++ CEN_ProjectResortArenaP-master/src/ResortArenaPalace/Main.java
@@ -1,9 +1,11 @@
 package ResortArenaPalace;
+import java.sql.DriverManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.Connection;
 
 /**
  * Project Resort Arena Palace.
@@ -20,9 +22,42 @@ public class Main extends Application {
         primaryStage.setTitle("Resort Arena Palace");
         primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
+        testDatabaseConnection();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
+
+
+
+
+
+    // Database Connection Test
+
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./res/test";
+    final String USER = "";
+    final String PASS = "";
+    private static Connection conn;
+
+    public void testDatabaseConnection(){
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Database Connected!");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Database Failed to Connect!");
+        }
+    }
+
+
+
 }
+
+
+
+
